f9fe48d877db531d2c659ac9eb922d71712ec6d5
Mykola Melnichuk <nikolfutdeveloper@gmail.com>
2025-02-05 21:40:42 +0100

    made solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,42 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [openModal, setOpenModal] = useState(false);
+  const [userId, setUserId] = useState<number>();
+  const [todo, setTodo] = useState<Todo>();
+  const [status, setStatus] = useState('all');
+  const [query, setQuery] = useState('');
+
+  const handleSelectStatus = (status: string = 'all') => {
+    setStatus(status);
+  };
+
+  const fetchTodos = (status?: string, query?: string) => {
+    getTodos(status, query)
+      .then(todos => setTodos(todos))
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    fetchTodos(status, query);
+  }, [query]);
+
+  useEffect(() => fetchTodos(status, query), [status]);
+
+  const close = () => setOpenModal(false);
+
+  const open = (todo: Todo, userId: number) => {
+    setUserId(userId);
+    setTodo(todo);
+    setOpenModal(true);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +51,32 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                handleSelectStatus={handleSelectStatus}
+                fetchTodos={fetchTodos}
+                setQuery={setQuery}
+                query={query}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  modalOpened={openModal}
+                  todos={todos}
+                  openModal={open}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {openModal && (
+        <TodoModal closeModal={close} userId={userId} todo={todo} />
+      )}
     </>
   );
 };
